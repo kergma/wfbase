0ecf1e3fce97ca891671226e292c7ef7a2972744
пушкинсв <pushkin@rosinv.ru>
2009-05-28 05:31:16 +0000

    отключил отладочный режим

--- web/lib/web.pm
+++ web/lib/web.pm
@@ -14,7 +14,7 @@ use Catalyst::Runtime '5.70';
 #                 directory
 
 use parent qw/Catalyst/;
-use Catalyst qw/-Debug
+use Catalyst qw/
                 ConfigLoader
                 Static::Simple
 		Authentication
