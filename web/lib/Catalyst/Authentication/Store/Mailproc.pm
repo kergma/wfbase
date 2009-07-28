3f7442eaf9ee090ee6a81b298a27703612ad8bd5
пушкинсв <pushkin@rosinv.ru>
2009-07-28 06:49:03 +0000

    Изменена категоризация сотрудников и отделений

--- web/lib/Catalyst/Authentication/Store/Mailproc.pm
+++ web/lib/Catalyst/Authentication/Store/Mailproc.pm
@@ -36,14 +36,14 @@ sub find_user
 	my ( $self, $authinfo, $c ) = @_;
 
 	my $dbh=DBI->connect("dbi:Pg:dbname=mailproc;host=localhost", undef, undef, {AutoCommit => 1}) or return 'Error connecting database' ;
-	my $sth=$dbh->prepare("select * from data where v2='$authinfo->{username}' and r like 'пароль%'");
-	$sth->execute(); 
+	my $sth=$dbh->prepare("select * from data where v2=? and r like 'пароль%'");
+	$sth->execute($authinfo->{username}); 
 	my $r=$sth->fetchrow_hashref;
 	$sth->finish();
 	$r and $authinfo->{password}=$r->{v1};
 
-	$sth=$dbh->prepare("select d2.v2 as full_name from data d1 join data d2 on d2.r like '%сотрудника' and d2.v1=d1.v2 where d1.v2='$authinfo->{username}'");
-	$sth->execute(); 
+	$sth=$dbh->prepare("select d2.v2 as full_name from data d1 join data d2 on d1.r like 'пароль%' and d2.r like '%сущности' and d2.v1=d1.v2 where d1.v2=?");
+	$sth->execute($authinfo->{username}); 
 	$r=$sth->fetchrow_hashref;
 	$sth->finish();
 	$r and $authinfo->{full_name}=$r->{full_name};
@@ -51,23 +51,17 @@ sub find_user
 	$sth=$dbh->prepare(qq/
 select d3.v1 as description
 from data d1
-join data d2 on d2.v1=d1.v2 
-join data d3 on d3.v2=d2.v2 and d3.r like 'описание%'
-where d1.v2='$authinfo->{username}' and d1.r like 'пароль%'
+join data d2 on d2.v1=d1.v2 and d2.r like '%сущности'
+join data d3 on d3.v2=d2.v2 and d3.r like 'описание сущности'
+where d1.v2=? and d1.r like 'пароль%'
 /
 );
-	$sth->execute(); 
+	$sth->execute($authinfo->{username}); 
 	while (my $r=$sth->fetchrow_hashref)
 	{
 		push @{$authinfo->{roles}}, split / /,$r->{description};
 	};
 	$sth->finish();
-
-	$sth=$dbh->prepare("select * from data where r like '%отделения' and v2='$authinfo->{username}'");
-	$sth->execute(); 
-	$r=$sth->fetchrow_hashref;
-	$sth->finish();
-	$r and push @{$authinfo->{roles}},'отделение';
 
 	$dbh->disconnect;
 	return bless $authinfo, 'Catalyst::Authentication::User::Hash';
