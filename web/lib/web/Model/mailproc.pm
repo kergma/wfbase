ef706af5d33ed15a02fed08ce9f850340c272aba
пушкинсв <pushkin@rosinv.ru>
2010-04-13 05:09:13 +0000

    Добавлены базовые методы модели данных mailproc

--- web/lib/web/Model/mailproc.pm
+++ web/lib/web/Model/mailproc.pm
@@ -3,6 +3,9 @@ package web::Model::mailproc;
 use strict;
 use warnings;
 use parent 'Catalyst::Model';
+use DBI;
+
+
 
 =head1 NAME
 
@@ -22,5 +25,36 @@ This library is free software, you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+
+my $dbh;
+
+sub connect
+{
+	$dbh and return $dbh;
+	$dbh=DBI->connect("dbi:Pg:dbname=mailproc;host=localhost", undef, undef, {AutoCommit => 1});
+	return $dbh;
+}
+
+sub array_ref
+{
+	my ($self, $q)=@_;
+	$self->connect() or return undef;
+
+	my $sth=$dbh->prepare($q);
+	$sth->execute();
+	my @result=();
+	while(my $r=$sth->fetchrow_arrayref)
+	{
+		push @result,$r->[0];
+	};
+	return \@result;
+}
+
+sub get_otd_list
+{
+	my ($self)=@_;
+	$self->connect() or return undef;
+	return array_ref($self,"select oti from orders where oti is not null group by oti order by oti");
+}
 
 1;
