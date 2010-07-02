a096e3bc490aaebb29d2f1e26670be2145f9f67c
пушкинсв <pushkin@rosinv.ru>
2010-07-02 03:40:17 +0000

    Добавлена модель для работы с данными по статистике

---
+++ web/lib/web/Model/stat.pm
@@ -0,0 +1,26 @@
+package web::Model::stat;
+
+use strict;
+use warnings;
+use parent 'Catalyst::Model';
+
+=head1 NAME
+
+web::Model::stat - Catalyst Model
+
+=head1 DESCRIPTION
+
+Catalyst Model.
+
+=head1 AUTHOR
+
+,,,
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
