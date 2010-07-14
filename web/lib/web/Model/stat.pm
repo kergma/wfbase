e5b67dedcd7a6a355884b9505dcbb3306694e46f
пушкинсв <pushkin@rosinv.ru>
2010-07-14 07:28:27 +0000

    Реализовано асинхроннное выполнение запросов статистики

--- web/lib/web/Model/stat.pm
+++ web/lib/web/Model/stat.pm
@@ -6,6 +6,8 @@ use parent 'Catalyst::Model';
 use DBI;
 use Encode;
 use Digest::MD5;
+use POSIX ":sys_wait_h";
+use Time::HiRes 'usleep';
 
 =head1 NAME
 
@@ -26,8 +28,14 @@ it under the same terms as Perl itself.
 
 =cut
 
-my $dbh;
 my $cc;
+
+#sub reaper
+#{
+#	#while ((my $child = waitpid(-1,WNOHANG)) > 0) {print "$$ reaped $child\n";undef $pid};
+#	1 while (my $child = waitpid(-1,WNOHANG)) > 0;
+#	$SIG{CHLD}=\&reaper;
+#}
 
 sub ACCEPT_CONTEXT
 {
@@ -39,49 +47,74 @@ sub ACCEPT_CONTEXT
 
 sub connect
 {
-	$dbh and return $dbh;
 	my $tmp=DBI->connect("dbi:Pg:dbname=mailproc;host=localhost", undef, undef, {AutoCommit => 1});
 	my $r=$tmp->selectrow_hashref("select v1 as password, v2 as username from data where r='пароль пользователя БД' and v2='stat'");
 	$tmp->disconnect;
-	$dbh=DBI->connect("dbi:Pg:dbname=mailproc;host=localhost", $r->{username}, $r->{password}, {AutoCommit => 1});
+	my $dbh=DBI->connect("dbi:Pg:dbname=mailproc;host=localhost", $r->{username}, $r->{password}, {AutoCommit => 1});
 	return $dbh;
 }
 
 sub query
 {
 	my ($self,$query)=@_;
-	$self->connect() or return undef;
 
 	my $qkey=Digest::MD5::md5_hex($query);
-	my $pending=$cc->cache->get($qkey);
-	if (defined $pending)
+	my $querying=$cc->cache->get($qkey);
+	if (defined $querying)
 	{
-		return {error=>'pending',pending=>$pending};
+		return {retrieval=>$querying->{retrieval}};
+	};
+
+	my $retrieval=Digest::MD5::md5_hex(rand());
+	my $start=time;
+
+	my $child=fork();
+
+	unless (defined $child)
+	{
+		return {error=>'cannot fork'};
+	};
+	if ($child)
+	{
+		#$SIG{CHLD}=\&reaper;
+		$querying={qkey=>$qkey,retrieval=>$retrieval,pid=>$child,start=>$start};
+		$cc->cache->set($qkey,$querying);
+		$cc->cache->set($retrieval,{qkey=>$qkey,retrieval=>$retrieval,query=>$query,querying=>$querying});
+		while ((time-$start)<5 and (my $c=waitpid($child,WNOHANG))>=0) {usleep(100)};
+		return {retrieval=>$retrieval};
 	};
 	
-	my $retrieval=Digest::MD5::md5_hex(rand());
-	$cc->cache->set($qkey,1);
+	my $dbh=$self->connect() or return undef;
 
-	my $start=time;
 	my $sth=$dbh->prepare($query);
-	$sth or return {error=>$dbh->errstr};
-	$sth->execute() or return {error=>$dbh->errstr};
-	my @rows;
-	while (my $r=$sth->fetchrow_hashref)
+	my $result={};
+	if ($sth and $sth->execute())
 	{
-		push @rows, {map {encode("utf8",$_) => $r->{$_}} keys %$r};;
-	}; 
+		my @rows;
+		while (my $r=$sth->fetchrow_hashref)
+		{
+			push @rows, {map {encode("utf8",$_) => $r->{$_}} keys %$r};;
+		}; 
 
-	my $result={qkey=>$qkey,rows=>\@rows, error=>$dbh->errstr, header=>[map(encode("utf8",$_),@{$sth->{NAME}})],duration=>time-$start, retrieved=>time, retrieval=>$retrieval};
+		$result={rows=>\@rows,header=>[map(encode("utf8",$_),@{$sth->{NAME}})]};
+	}
+	$result={%$result,(query=>$query,duration=>time-$start,retrieved=>time,retrieval=>$retrieval,error=>$dbh->errstr)};
+	#$result={%$result,(query=>$query,qkey=>$qkey,duration=>time-$start,retrieved=>time,retrieval=>$retrieval,error=>$dbh->errstr)};
 	$cc->cache->remove($qkey);
+	$cc->cache->set($retrieval,$result);
+	$dbh->disconnect();
 
-	return $result;
+	exit(0);
 
 }
 
 sub result
 {
-	my ($retrieval,$start,$count)=@_;
+	my ($self,$retrieval,$start,$count)=@_;
+	my $result=$cc->cache->get($retrieval);
+	$result->{querying}->{duration}=time-$result->{querying}->{start} if $result and defined $result->{querying};
+	return $result if $result;
+	return {error=>'Неправильный или устаревший идентификатор запроса'};
 }
 
 1;
