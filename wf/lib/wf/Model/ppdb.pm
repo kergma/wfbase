0081797e4476245958dc87093995e31456fb95eb
пушкинсв <pushkin@rosinv.ru>
2013-05-13 08:36:40 +0000

    изготовление техпланов через ordcard.tt

--- wf/lib/wf/Model/ppdb.pm
+++ wf/lib/wf/Model/ppdb.pm
@@ -1377,61 +1377,45 @@ sub orders_being_processed
 
 	my $self=shift;
 	my $operator=shift;
-
-	my @a;
-	my $r=db::selectall_arrayref(qq/
-select 'assigned' as rtype, o.id as order_id,o.ordno, o.objno,o.year,j.id as object_id, j.address,
-(select v1 from data where r='наименование структурного подразделения' and v2=o.sp::text) as spname,
-(select v1 from data where r='код структурного подразделения' and v2=o.sp::text) as spcode
-from log l
-join packets p on p.id=l.refid
-join orders o on o.id=p.order_id
-join objects j on j.id=o.object_id
-where l.who=? and event='назначен' and refto='packets'
-and not exists (select 1 from log where refid=l.refid and id>l.id)
-order by p.id desc
-/, {Slice=>{}},$operator);
-	return {error=>$DBI::errstr} unless $r;
-	push @a, @$r;
+	my $filter=shift;
 	
-	$r=db::selectall_arrayref(qq/
-select 'accepted' as rtype, o.id as order_id,o.ordno, o.objno,o.year,j.id as object_id, j.address,
-(select v1 from data where r='наименование структурного подразделения' and v2=o.sp::text) as spname,
-(select v1 from data where r='код структурного подразделения' and v2=o.sp::text) as spcode
-from (
+	my $inner=qq\
 select o.id,o.sp,o.ordno,o.year,o.objno,o.object_id,max(l.id) as event_id
 from log l
-join packets p on p.id=l.refid and l.refto='packets' and l.event='принят'
+join packets p on p.id=l.refid and l.refto='packets'
 left join orders o on (o.id=l.refid and l.refto='orders') or o.id=p.order_id
 where l.who=?
 and (select event from log where refto='orders' and refid=o.id order by id desc limit 1)<>'закрыт'
 group by o.id,o.sp,o.ordno,o.year,o.objno,o.object_id
+\;
+
+	my @a;
+	my $r;
+	$r=db::selectall_arrayref(qq/
+select o.id as order_id,o.ordno, o.objno,o.year,j.id as object_id, j.address, sp,
+(select v1 from data where r='наименование структурного подразделения' and v2=o.sp::text) as spname,
+(select v1 from data where r='код структурного подразделения' and v2=o.sp::text) as spcode
+from (
+$inner
 order by event_id desc
 limit 8
 ) o 
 join objects j on j.id=o.object_id
-/, {Slice=>{}},$operator);
+/, {Slice=>{}},$filter||$operator);
 	return {error=>$DBI::errstr} unless $r;
 	push @a, @$r;
 
 	foreach my $o (@a)
 	{
-		$o->{packets}=db::selectall_arrayref("select * from packets p where order_id=? order by p.id desc",{Slice=>{}},$o->{order_id});
-		return {error=>$DBI::errstr} unless $o->{packets};
+		my $r=order_data($self,$o);
+		return $r if $r->{error};
+		my $p=$o->{packets}[0];
+		$o->{group}='к принятию' if $p->{type} eq 'данные' and $p->{status}->{event} eq 'назначен' and ($filter ne $operator or $p->{status}->{who} eq $operator);
 
-
-		$_->{file}=storage::tree_of($_->{container},\@{$_->{filelist}}) foreach @{$o->{packets}};
-		foreach (@{$o->{packets}})
-		{
-			$_->{status}=db::selectrow_hashref(qq/
-select event,note,to_char(date,'yyyy-mm-dd hh24:mi') as datef, coalesce(d.v1,l.who::text) as fio
-from log l 
-left join data d on d.r='ФИО сотрудника' and v2=l.who::text
-where refto='packets' and refid=? order by l.id desc, d.id desc limit 1
-/,undef,$_->{id});
-			return {error=>$DBI::errstr} unless defined $_->{status};
-		};
+		$_->{file}=storage::tree_of($_->{container},\@{$_->{filelist}}) foreach $o->{group}?($o->{packets}->[0]):@{$o->{packets}};
 	};
+	my %group_ordering=('замечания'=>1,'к принятию'=>2,'техплан'=>3,''=>4,'к закрытию'=>5);
+	@a=sort {$group_ordering{$a->{group}} <=> $group_ordering{$b->{group}}} @a;
 
 	return { ARRAY=>\@a, };
 }
